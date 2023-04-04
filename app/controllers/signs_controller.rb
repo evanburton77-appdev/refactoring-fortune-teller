c88dfa2d17fb16e2c351f060717e49173007ce09
Evan Burton <evanburton77@gmail.com>
2023-04-04 23:39:44 +0000

    complete

---
+++ app/controllers/signs_controller.rb
@@ -0,0 +1,18 @@
+class SignsController < ApplicationController
+  def sign
+    @sign = params.fetch("sign")
+    all_zodiacs = Zodiac.list
+    this_zodiac = all_zodiacs.fetch(@sign.to_sym)
+    @horoscope = this_zodiac.fetch(:horoscope)
+
+    @array_of_numbers = Array.new
+
+    5.times do
+      another_number = rand(1...100)
+
+      @array_of_numbers.push(another_number)
+    end
+
+    render({ :template => "zodiac_templates/zodiac.html.erb" })
+  end
+end
